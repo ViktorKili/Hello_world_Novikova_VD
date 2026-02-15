ea4064189c01afccdf59bf6537f0612cf8827e00
ViktorKili <viktor.kili2606@gmail.com>
2026-02-15 20:12:16 +0300

    Практика 2.2

---
+++ Лабораторная2/task_2-2_inventory_control.py
@@ -0,0 +1,8 @@
+user_input = input("Напиши название реактива")
+processed_input = user_input
+user_input2 = input("Сколько реактива в наличии")
+processed_input = user_input2
+
+print(f"Название реактива: {user_input}")
+print(f"Количество: {user_input2}")
+print(f"Реактив {user_input} поступил на склад в количестве {user_input2}")
