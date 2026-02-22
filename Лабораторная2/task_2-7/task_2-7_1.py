54a0c2c16705bd8267f727b906d4f4bb360fe15e
ViktorKili <viktor.kili2606@gmail.com>
2026-02-22 21:03:16 +0300

    Add files via upload

---
+++ Лабораторная2/task_2-7/task_2-7_1.py
@@ -0,0 +1,5 @@
+files = ["seq1", "seq2", "seq3" ]
+for name in files:
+    new_name = name + ".fasta\t Дата взятия: 22.02.2026"
+
+print(f"{new_name}")
---
+++ Лабораторная2/task_2-7/task_2-7_3.py
@@ -0,0 +1,8 @@
+seq = [ "ATATACGCGTA", "CTTCGGNGGA" ]
+a = str(seq[0]) + str(seq[1])
+print(a)
+for l in a:
+
+    print(l)
+
+print("Цикл выполнен")
